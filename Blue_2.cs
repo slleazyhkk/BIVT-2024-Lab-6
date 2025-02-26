e4781d9fd5b1fbde57c7f06ba895eb3f51caed08
slleazyhkk <m2408026@edu.misis.ru>
2025-02-26 19:24:11 +0300

    Update Blue_2.cs

--- Blue_2.cs
+++ Blue_2.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab_6
+namespace ConsoleApp6
 {
     public class Blue_2
     {
@@ -23,10 +23,10 @@ namespace Lab_6
                 get
                 {
                     if (_marks == null) return null;
-                    int[,] copyMarks = new int[_marks.GetLength(0), _marks.GetLength(1)];
-                    for (int i = 0; i < _marks.GetLength(0); i++)
+                    int[,] copyMarks = new int[2,5];
+                    for (int i = 0; i < 2; i++)
                     {
-                        for (int j = 0; j < _marks.GetLength(1); j++)
+                        for (int j = 0; j < 5; j++)
                         {
                             copyMarks[i, j] = _marks[i, j];
                         }
@@ -38,10 +38,11 @@ namespace Lab_6
             {
                 get
                 {
+                    if (_marks==null) return 0;
                     int s = 0;
-                    for (int i = 0; i < _marks.GetLength(0); i++)
+                    for (int i = 0; i < 2; i++)
                     {
-                        for (int j = 0; j < _marks.GetLength(1); j++)
+                        for (int j = 0; j < 5; j++)
                         {
                             s += _marks[i, j];
                         }
@@ -54,12 +55,12 @@ namespace Lab_6
             {
                 _name = name;
                 _surname = surname;
-                _marks = new int[2, 5];
+                _marks = new int[2,5];
             }
             //method
             public void Jump(int[] result)
             {
-                if (result == null || result.Length != 5) return;
+                if (result == null || _marks == null) return;
                 for (int i = 0; i < 2; i++)
                 {
                     if (_marks[i, 0] == 0)
@@ -75,6 +76,7 @@ namespace Lab_6
 
             public static void Sort(Participant[] array)
             {
+                if (array == null || array.Length==0) return;
                 for (int i = 0; i < array.Length; i++)
                 {
                     for (int j = 0; j < array.Length - i - 1; j++)
